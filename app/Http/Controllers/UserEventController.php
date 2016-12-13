40fd9bb5c0bb7e1a16883dfabbef3d8a7fbca4c2
Deoval Junior <deovaljr@gmail.com>
2016-12-13 17:38:49 -0200

    commitando vendor

--- app/Http/Controllers/UserEventController.php
+++ app/Http/Controllers/UserEventController.php
@@ -16,8 +16,8 @@ class UserEventController extends Controller
     public function getUserEvents(Request $request)
     {
         //$user_id = $request->get('user_id');
-        return response()->json(['req'=>$request]);
         $access_token = $request->get('access_token');
+        return $access_token;
         //$url = '/' . $user_id . '/events';
         $url = '/me/events';
         dd($url);
