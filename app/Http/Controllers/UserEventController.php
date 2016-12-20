6d8b779a913af28161ece64fa2f5dc980459cdce
Deoval Junior <deovaljr@gmail.com>
2016-12-20 15:50:39 -0200

    Filtrando eventos do usuario e pegando imagens

--- app/Http/Controllers/UserEventController.php
+++ app/Http/Controllers/UserEventController.php
@@ -17,30 +17,10 @@ class UserEventController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    /*public function getUserEvents(Request $request)
-    {
-        //$user_id = $request->get('user_id');
-        $access_token = $request->get('access_token');
-        //return $access_token;
-        //$url = '/' . $user_id . '/events';
-        $url = '/me/events';
-        //dd($url);
-        $fb = new \Facebook\Facebook(['app_id' => '1379982378719884',
-		  'app_secret' => '6de34ab5bb6310b76e57cb18d677643b',
-		  'default_graph_version' => 'v2.8',
-		]);
-		$response = $fb->get($url, $access_token);
-
-		$arrayResult = json_decode($response->getBody(), true);
-		
-		return response()->json(['data'=>$arrayResult['data']]);		
-    }*/
-
     public function getUserEvents(Request $request)
     {
-        //$access_token = $request->get('access_token');
-        $access_token = 'EAATnFi962owBALwZChcQmJz2BX0X3FzebBi7YCaVELqtIjI7Dj4SCqTHGwvzxbPjuRN0XoaOfTbYkNnjpz9BJ2De2ViK52OheIUDNhfqEhkEFQXcdjudn2X3WIUhW0L5M8WGDrKxh6R8PGwoC6SErIllsfR3e6Q7ccT313wZDZD';
-        $url = '/me/events';
+        $access_token = $request->get('access_token');
+        $url = '/me/events?fields=id,name,description,start_time,end_time,place,cover';
         $fb = new \Facebook\Facebook(['app_id' => '1379982378719884',
 		  'app_secret' => '6de34ab5bb6310b76e57cb18d677643b',
 		  'default_graph_version' => 'v2.8',
@@ -54,8 +34,8 @@ class UserEventController extends Controller
 			$event_exists = Event::where('facebook_event_id', '=', $event['id'])->first();
 			if (is_null($event_exists)) {
 				$events[$i] = $event;
+				$i++;
 			}
-			$i++;
 		}		
 		return response()->json(['data'=>$events]);		
     }
